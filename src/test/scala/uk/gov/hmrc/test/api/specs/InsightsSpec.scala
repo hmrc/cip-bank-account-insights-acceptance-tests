8fb478024d3bf4e32e35a773c6004b756d5b5d44
Mark Collin <1205116+Ardesco@users.noreply.github.com>
2022-07-05 09:22:25 +0100

    #TAV-603 Change upper limit expectation from 99 to 100

--- src/test/scala/uk/gov/hmrc/test/api/specs/InsightsSpec.scala
+++ src/test/scala/uk/gov/hmrc/test/api/specs/InsightsSpec.scala
@@ -18,15 +18,15 @@ package uk.gov.hmrc.test.api.specs
 
 import com.github.tomakehurst.wiremock.client.WireMock.{matchingJsonPath, postRequestedFor, urlEqualTo, verify}
 import org.assertj.core.api.Assertions.assertThat
-import uk.gov.hmrc.insights.model.response.response_codes.ACCOUNT_NOT_ON_WATCH_LIST
+import uk.gov.hmrc.insights.model.response.response_codes.{ACCOUNT_NOT_ON_WATCH_LIST, ACCOUNT_ON_WATCH_LIST}
 import uk.gov.hmrc.test.api.conf.TestConfiguration
-import uk.gov.hmrc.test.api.testdata.BankAccounts.UNKNOWN_ACCOUNT
+import uk.gov.hmrc.test.api.testdata.BankAccounts.{RISKY_ACCOUNT, UNKNOWN_ACCOUNT}
 
 class InsightsSpec extends BaseSpec with WireMock {
 
   Feature("Check the insight API") {
 
-    Scenario("Get risking information for a bank account") {
+    Scenario("Get risking information for an UNKNOWN bank account") {
       Given("I want to see if we hold any risking information for a bank account")
 
       When("I use the check insights API to see what information we hold")
@@ -46,6 +46,37 @@ class InsightsSpec extends BaseSpec with WireMock {
                 s"@.auditSource == '${TestConfiguration.expectedServiceName}'" +
                 "&& @.auditType == 'TxSucceeded'" +
                 s"&& @.detail.accountNumber == '${UNKNOWN_ACCOUNT.accountNumber}'" +
+                s"&& @.detail.bankAccountInsightsCorrelationId == '${actual.bankAccountInsightsCorrelationId}'" +
+                s"&& @.detail.riskScore == '${actual.riskScore}'" +
+                s"&& @.detail.reason == '${actual.reason}'" +
+                s"&& @.detail.userAgent == '${TestConfiguration.userAgent}'" +
+                ")]"
+            )
+          )
+      )
+    }
+
+    //TODO Enable when this functionality has been added
+    ignore("Get risking information for a bank account on the risk list") {
+      Given("I want to see if we hold any risking information for a bank account")
+
+      When("I use the check insights API to see what information we hold")
+
+      val actual = accountCheckHelper.getAccountCheckResponse(RISKY_ACCOUNT)
+
+      Then("I am given the relevant risking information")
+
+      assertThat(actual.riskScore).isEqualTo(100)
+      assertThat(actual.reason).isEqualTo(ACCOUNT_ON_WATCH_LIST)
+
+      verify(
+        postRequestedFor(urlEqualTo("/write/audit"))
+          .withRequestBody(
+            matchingJsonPath(
+              "$[?(" +
+                s"@.auditSource == '${TestConfiguration.expectedServiceName}'" +
+                "&& @.auditType == 'TxSucceeded'" +
+                s"&& @.detail.accountNumber == '${RISKY_ACCOUNT.accountNumber}'" +
                 s"&& @.detail.bankAccountInsightsCorrelationId == '${actual.bankAccountInsightsCorrelationId}'" +
                 s"&& @.detail.riskScore == '${actual.riskScore}'" +
                 s"&& @.detail.reason == '${actual.reason}'" +
